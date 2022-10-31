2693864754bf95f7b4a3014fe32bb351805a5137
subin <newdeas100@gmail.com>
2022-11-01 02:45:27 +0900

    feat(FE-cart) : 로컬 storage에서 데이터받아서 렌더링하는 기능추가

---
+++ src/views/cart/cart.js
@@ -0,0 +1,46 @@
+import { setLocal } from "./util/util.js"
+
+async function fetchData() {
+    const res = await fetch("./data/data.json");
+    const data = await res.json();
+    setLocal(data);
+    console.log(data);
+    return data;
+}
+
+function renderData(datas) {
+    const itemContainer = document.querySelector(".item-container");
+    const dataString = htmlTemplate(datas);
+    itemContainer.innerHTML += dataString;
+}
+
+function htmlTemplate(datas){
+
+    return datas.items.map(data =>{
+
+        const {itemName, itemPrice, itemNumber, itemImg, itemAuthor} = data;
+
+        return `<div class="itembox">
+        <div class="imgbox">
+          <button id="select_btn"></button>
+          <div>${itemImg}</div>
+        </div>
+        <div class="infoBox">
+          <span>${itemName}/ ${itemAuthor}</span>
+          <div class="add_btn_container">
+            <button class="add_btn">🔺</button>
+            <input type="text" value="1" class="add_btn_input" />
+            <button class="add_btn">🔻</button>
+          </div>
+        </div>
+        <div class="priceBox">
+          <div>금액</div>
+          <div>${itemPrice}</div>
+        </div>
+        </div>`
+    })
+}
+
+
+fetchData()
+    .then(data => renderData(data))
---
+++ src/views/cart/util/util.js
@@ -0,0 +1,3 @@
+export function setLocal(data) {
+  localStorage.setItem("bookInfo", JSON.stringify(data));
+}
