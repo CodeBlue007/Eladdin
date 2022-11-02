1db094f74032f5fcdd815850d857eaca7c456ad3
chloeseo0210 <chloeseo0210@gmail.com>
2022-11-02 12:20:27 +0900

    feat : bookAdmin API

--- src/routers/book-router.js
+++ src/routers/book-router.js
@@ -8,14 +8,12 @@ const bookRouter = Router();
 //eladin.com/books
 bookRouter.get("/", async (req, res) => {
     const books = await bookService.findAll();
-
     res.json(books);
   }
 );
 
 bookRouter.get("/:ISBN", async (req, res) => {
   const ISBN = parseInt(req.params.ISBN)
-
   const book = mockBookDB.find(book=> book.ISBN === ISBN);
   console.log(book)
   res.json(book);
@@ -24,10 +22,25 @@ bookRouter.get("/:ISBN", async (req, res) => {
 
 // TODO: 로그인 기능 구현 시 middleware 추가
 bookRouter.post("/", async (req, res) => {
-    mockBookDB.push(req.body)  
-
-    res.send(mockBookDB);
+    const newBook = req.body;
+    await bookService.create(newBook);
   }
 );
+
+bookRouter.put("/:ISBN", async (req, res) => {
+  const ISBN = parseInt(req.params.ISBN)
+  const bookInfo = req.body
+  await bookService.update({ISBN, bookInfo})
+}
+);
+
+
+bookRouter.delete("/:ISBN", async (req, res) => {
+  const ISBN = parseInt(req.params.ISBN)
+  await bookService.deleteByISBN(ISBN)
+}
+);
+
+
 
 export { bookRouter };
