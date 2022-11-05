8dfee9e964c2177a848cf136a4bd8024aa1e9cdd
dongjooyun <dongjooyun@khu.ac.kr>
2022-11-06 00:55:43 +0900

    Style(FE-categoryBooks): commit for merge

--- src/views/category-booklist/subCategory-booklist.js
+++ src/views/category-booklist/subCategory-booklist.js
@@ -13,7 +13,7 @@ async function App() {
         const data = await fetchData();
         let { category } = data;
         addEventListeners(data);
-        renderBookData(data, category = '컴퓨터/모바일'); // 여기서 실행되지 않고 각 카테고리 클릭 시 실행하려고 함.
+        renderBookData(data, category = '여행'); // 여기서 실행되지 않고 각 카테고리 클릭 시 실행하려고 함.
     } catch (err) {
         console.log(err);
     }
