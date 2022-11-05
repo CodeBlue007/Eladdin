a5dc67621c1fbfe817e08bee8a40dd9ccdc3e976
Yoo Jeong KIm <jeongola@naver.com>
2022-11-06 02:20:47 +0900

    feat(FE-admin-page): 카테고리 관리 수정 & 제품 설정 UI 수정

--- src/views/admin-page/admin-page-product.js
+++ src/views/admin-page/admin-page-product.js
@@ -19,17 +19,15 @@ function htmlTemplate(datas) {
       return `
       <div class="product-list">
         <img class="book-img" src=${imgUrl} width="50"/>
-        <ul class="products">
-          <li class="book-title">${title}</li>
-          <li class="EBook">${EBook}</li>
-          <li class="author">${author}</li>
-          <li class="publisher">${publisher}</li>
-          <li class="intro">${description}</li>
-          <li class="price">${price}</li>
-          <li class="publicationDate">${publicationDate}</li>
-          <button class="edit-btn">수정</button>
-          <button class="delete-btn">삭제</button>
-        </ul>
+        <div class="book-title">${title}</div>
+        <div class="EBook">${EBook}</div>
+        <div class="author">${author}</div>
+        <div class="publisher">${publisher}</div>
+        <div class="intro">${description}</div>
+        <div class="price">${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</div>
+        <div class="publicationDate">${publicationDate}</div>
+        <button class="edit-btn">수정</button>
+        <button class="delete-btn">삭제</button>
       </div>
     `;
     })
