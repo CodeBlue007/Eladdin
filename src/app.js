3509b0e9f7979f854a6468e766016448aff4c8a5
hyemin <kimhm1101@gmail.com>
2022-11-06 17:27:44 +0900

    fix : user schema

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ app.use("/api/auth", userRouter);
 app.use("/api/loadimg", imgRouter);
 app.use("/api/books", bookRouter);
 app.use("/api/order", orderRouter);
+app.use("/api/category", categoryRouter);
 
 // 순서 중요 (errorHandler은 다른 일반 라우팅보다 나중에 있어야 함)
 // 그래야, 에러가 났을 때 next(error) 했을 때 여기로 오게 됨
