ccba50f743bde6d1e86196e8ea8905e0380a0ecf
subin <newdeas100@gmail.com>
2022-11-10 00:43:42 +0900

    feat(admin) : 카테고리 수정기능 추가

--- src/views/api.js
+++ src/views/api.js
@@ -89,7 +89,7 @@ async function patch(endpoint, params = '', data) {
 // 아래 함수명에 관해, delete 단어는 자바스크립트의 reserved 단어이기에,
 // 여기서는 우선 delete 대신 del로 쓰고 아래 export 시에 delete로 alias 함.
 
-async function put(endpoint, params = '', data) {
+async function put(endpoint, params = '' ,data={}) {
   const apiUrl = `${endpoint}/${params}`;
 
   // JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
@@ -150,4 +150,4 @@ async function del(endpoint, params = '', data = {}) {
 }
 
 // 아래처럼 export하면, import * as Api 로 할 시 Api.get, Api.post 등으로 쓸 수 있음.
-export { get, post, patch,del,put as delete };
+export { get, post, patch, put, del as delete };
