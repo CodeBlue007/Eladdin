b42bd8ec0078fdfd5f6ceb178056ab40b43f071b
subin <newdeas100@gmail.com>
2022-11-11 03:39:21 +0900

    fix(cart) 로고클릭시 경로수정, 주문목록 버그고침

--- src/views/my-page/component/eventFn/deleteOrder.js
+++ src/views/my-page/component/eventFn/deleteOrder.js
@@ -5,7 +5,8 @@ export async function deleteOrder(e){
 
     const {id:orderId, shippingStatus} = e.target.dataset;
 
-    const container = document.getElementById(orderId);
+    const item_container = document.getElementById(orderId);
+    const order_container = document.querySelector(".order-products")
 
     if (shippingStatus !== "배송준비중") {
         alert('배송준비중 일 때에만 취소가 가능합니다!');
@@ -16,5 +17,7 @@ export async function deleteOrder(e){
     await Api.delete(`https://eladin-lgurfdxfjq-du.a.run.app/api/order/${orderId}`)
 
     alert('취소되었습니다!');
-    container.remove();
+    item_container.remove();
+    order_container.classList.add("hidden");
+
 }
