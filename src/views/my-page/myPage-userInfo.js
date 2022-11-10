20dd21a45b6699740310b46fbf9f88d250e7a927
chloeseo0210 <chloeseo0210@gmail.com>
2022-11-10 15:48:03 +0900

    feat : userInfo markup 수정

--- src/views/my-page/myPage-userInfo.js
+++ src/views/my-page/myPage-userInfo.js
@@ -1,70 +1,70 @@
-async function fetchData() {
-  const res = await fetch('./data/data.json');
-  const data = await res.json();
-  console.log(data);
-  return data;
-}
+// async function fetchData() {
+//   const res = await fetch('./data/data.json');
+//   const data = await res.json();
+//   console.log(data);
+//   return data;
+// }
 
-function renderData(datas) {
-  const userInfo = document.querySelector('.userInfo');
-  const dataString = htmlTemplate(datas);
-  userInfo.innerHTML += dataString;
-}
+// function renderData(datas) {
+//   const userInfo = document.querySelector('.userInfo');
+//   const dataString = htmlTemplate(datas);
+//   userInfo.innerHTML += dataString;
+// }
 
-function htmlTemplate(datas) {
-  return datas.map((data) => {
-    const { email, fullName, phoneNumber, address } = data;
+// function htmlTemplate(datas) {
+//   return datas.map((data) => {
+//     const { email, fullName, phoneNumber, address } = data;
 
-    return `
-      <table class="user-info">
-        <tbody>
-          <tr>
-            <th scope="row">이메일</th>
-            <td>
-              <div class="usermodity-email">
-                ${email} &nbsp;&nbsp;
-                <button> 이메일 변경 </button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">이름</th>
-            <td>
-              <div class="usermodity-name">
-                ${fullName} &nbsp;&nbsp;
-                <button> 이름 변경 </button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">휴대폰 번호</th>
-            <td>
-              <div class="usermodity-phoneNumber">
-                ${phoneNumber} &nbsp;&nbsp;
-                <button> 휴대폰 번호 변경 </button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">비밀 번호</th>
-            <td>
-              <div class="usermodity-phoneNumber">
-                <button> 비밀 번호 변경 </button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">주소 변경</th>
-            <td>
-              <div class="usermodity-address">
-              ${address} &nbsp;&nbsp;
-                <button> 주소 변경 </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>`;
-  });
-}
+//     return `
+//       <table class="user-info">
+//         <tbody>
+//           <tr>
+//             <th scope="row">이메일</th>
+//             <td>
+//               <div class="usermodity-email">
+//                 ${email} &nbsp;&nbsp;
+//                 <button> 이메일 변경 </button>
+//               </div>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">이름</th>
+//             <td>
+//               <div class="usermodity-name">
+//                 ${fullName} &nbsp;&nbsp;
+//                 <button> 이름 변경 </button>
+//               </div>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">휴대폰 번호</th>
+//             <td>
+//               <div class="usermodity-phoneNumber">
+//                 ${phoneNumber} &nbsp;&nbsp;
+//                 <button> 휴대폰 번호 변경 </button>
+//               </div>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">비밀 번호</th>
+//             <td>
+//               <div class="usermodity-phoneNumber">
+//                 <button> 비밀 번호 변경 </button>
+//               </div>
+//             </td>
+//           </tr>
+//           <tr>
+//             <th scope="row">주소 변경</th>
+//             <td>
+//               <div class="usermodity-address">
+//               ${address} &nbsp;&nbsp;
+//                 <button> 주소 변경 </button>
+//               </div>
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>`;
+//   });
+// }
 
-fetchData().then((data) => renderData(data));
+// fetchData().then((data) => renderData(data));
