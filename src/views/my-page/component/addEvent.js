b2a2e6c6f6c833dad81f607255ab8f237afeda87
subin <newdeas100@gmail.com>
2022-11-11 00:23:09 +0900

    feat(my page) : 회원탈퇴 기능 추가

--- src/views/my-page/component/addEvent.js
+++ src/views/my-page/component/addEvent.js
@@ -1,5 +1,6 @@
 import {closeModal, showModal} from "./eventFn/modalFn.js";
 import { checkPw } from "./eventFn/checkPw.js";
+import { deleteUser } from "./eventFn/deleteUser.js";
 
 
 export function addEvent(){
@@ -9,11 +10,13 @@ const userInfo_btn = document.querySelector('.userInfo_btn');
 const orderList_btn = document.querySelector('.order-list');
 const confirm_btn = document.querySelector('.confirm-btn');
 const modal_x_btn = document.querySelector('.modal-close');
+const deleteUser_Btn = document.querySelector(".deleteAccount");
 
 // orderList_btn.addEventListener('click', showOrderList);
 userInfo_btn.addEventListener('click', showModal);
 modal_x_btn.addEventListener('click', closeModal);
 
 confirm_btn.addEventListener("click", checkPw);
+deleteUser_Btn.addEventListener("click", deleteUser);
 
 }
